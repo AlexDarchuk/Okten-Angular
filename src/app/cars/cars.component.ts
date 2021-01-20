bdbaba5f43f78e8e89e0ba5059d3071e050c3bfb
Alex <o.darchuk@gmail.com>
2021-01-20 12:30:33 +0200

    angular-lesson-1

---
+++ src/app/cars/cars.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {cars} from '../../data/dataCars';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+export class CarsComponent implements OnInit {
+
+  cars: any[] = cars;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/data/dataCars.ts
@@ -0,0 +1,12 @@
+export const cars = [
+    {production: 'toyota', power: 67, color: 'black', year: 2010},
+    {production: 'mazda', power: 86, color: 'gray', year: 2015},
+    {production: 'opel', power: 90, color: 'red', year: 2013},
+    {production: 'bmw', power: 70, color: 'black', year: 2009},
+    {production: 'citroen', power: 83, color: 'gray', year: 2017},
+    {production: 'renault', power: 97, color: 'red', year: 2019},
+    {production: 'honda', power: 80, color: 'black', year: 2000},
+    {production: 'kia', power: 65, color: 'gray', year: 2016},
+    {production: 'audi', power: 91, color: 'red', year: 2004},
+    {production: 'mercedes', power: 100, color: 'black', year: 2017}
+];
