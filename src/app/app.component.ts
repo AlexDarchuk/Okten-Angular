c57c38e12fa1b99f4a38d4ac869707f0bbf24da8
Your Name <o.darchuk@gmail.com>
2021-01-29 18:45:45 +0200

    angular-lesson-5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular';
+  //  user = {name: 'vasya',age: 45};
+  //
+  // submit(form: NgForm): void {
+  //   console.log(form);
+  // }
+  name = new FormControl('', [Validators.required, Validators.minLength(10), this.customFukcValidator]);
+  age = new FormControl('');
+  myForm = new FormGroup({
+    name: this.name,
+    age: this.age
+  });
+  xxx(myForm: FormGroup): void {
+    console.log(myForm);
+  }
+  customFukcValidator(inputData: AbstractControl): any {
+    console.log('test');
+    if (inputData.value === 'fukc') {
+      console.log(inputData);
+      return {error: true, msg: 'fuck is present'};
+    } else {
+      return null;
+    }
+  }
 }
