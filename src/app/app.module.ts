006dc45c0a56006db76edbb3e61f1eb747a1c2ae
Your Name <o.darchuk@gmail.com>
2021-02-01 12:51:07 +0200

    angular-lesson-6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {UserModule} from './user/user.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{
+      path: 'users', loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+    }])
   ],
   providers: [],
   bootstrap: [AppComponent]
